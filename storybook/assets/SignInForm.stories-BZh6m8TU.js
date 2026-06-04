56ae939c2084ada4c5f40d0c0250f136f89f791b
lyubomyr-rudko <lyubomyr-rudko@users.noreply.github.com>
2026-06-04 14:35:50 +0000

    deploy: lyubomyr-rudko/stronger-app@2fe83f0544dbee05c022805133181aa7fee78924

---
+++ storybook/assets/SignInForm.stories-BZh6m8TU.js
@@ -0,0 +1,9 @@
+import{n as e}from"./chunk-DnJy8xQt.js";import{a as t,i as n,n as r,o as i,r as a,t as o}from"./iframe-DG7cgRuk.js";import{n as s,t as c}from"./SignInForm-0S6a79hk.js";var l,u,d,f,p;e((()=>{s(),r(),l=i(),u={title:`Components/SignInForm`,component:c,decorators:[e=>(0,l.jsx)(o,{children:(0,l.jsx)(`div`,{className:`mx-auto max-w-[460px]`,children:(0,l.jsx)(e,{})})})]},d={decorators:[t({auth:n})]},f={decorators:[t({auth:a})]},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`{
+  decorators: [withStoryStore({
+    auth: signedOutAuthState
+  })]
+}`,...d.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`{
+  decorators: [withStoryStore({
+    auth: signedInAuthState
+  })]
+}`,...f.parameters?.docs?.source}}},p=[`SignedOut`,`SignedIn`]}))();export{f as SignedIn,d as SignedOut,p as __namedExportsOrder,u as default};
